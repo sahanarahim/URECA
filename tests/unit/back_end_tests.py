5c19797fd8db9a04ea85d330d887b0eeb18ae40f
kevfo <75442525+kevfo@users.noreply.github.com>
2023-05-29 02:55:56 +0800

    Removed previous tests
    I removed the tests that were previously there as things have changed.  New tests will (hopefully) be added in due time.

--- tests/unit/back_end_tests.py
+++ tests/unit/back_end_tests.py
@@ -8,22 +8,11 @@ import sys
 sys.path.append(os.getcwd())
 
 # Importing user modules:
-import utils.edges as e
+import utils.search as e
 from app import app
 
 class BackEndTestCases(unittest.TestCase):
-    def setUp(self):
-        app.testing = True
-        self.app = app
-        self.example_values = [('Tomato levels', 'INCREASED BY', 'Ketchup'), 
-                               ('Mustard', 'CAUSES', 'indigestion'),
-                               ('Chocolate cravings', 'RepResseD BY', 'wilLpower'),
-                               ('GeNOType', 'MODulated by', 'stuff')]
-        
-    def test_search_passive_edges_should_return_right_edge(self):
-        answers = ['INCREASES', None, 'REPRESSES', 'MODULATES']
-        for i in range(len(self.example_values)):
-            self.assertEqual(e.search_passive_edges(self.example_values[i][1]), answers[i])
+    pass
 
 if __name__ == '__main__':
     unittest.main()
