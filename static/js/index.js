40589a5d5ddaf13166816026f007409494e30906
kevfo <75442525+kevfo@users.noreply.github.com>
2023-05-29 02:55:04 +0800

    Added functionality for viewing instructions
    I wrote some vanilla JS to display help instructions based on what the user wants to search for.

--- static/js/index.js
+++ static/js/index.js
@@ -26,3 +26,40 @@ function submitFormWith(event, value) {
     input.value = value;
     form.submit();
 }
+
+/*
+This part of the file contains JavaScript for the help part of the landing page.
+*/
+
+// Selects the necessary HTML elements to change / attach event listeners to:
+const help_information = document.querySelector('#help-text');
+const buttons = document.querySelector('.button-group.hollow').querySelectorAll('.button');
+
+// Change help information depending on which button is clicked:
+const change_help_text = (text) => {
+    let help_text = '';
+    switch (text) {
+        case 'exact':
+            help_text = 'Finds entities containing the exact term (e.g., searching for "cesa" would return "cesa expression", "cesa tracking", and so on).';
+            break;
+        case 'alias':
+            help_text = 'Finds gene aliases (e.g., searching for "cesa1" finds terms such as "rsw1", "atcesa1", and "any1").';
+            break;
+        case 'substring':
+            help_text = 'Finds entities containing the substring (e.g., searching for "hair" would return "root hairs", "hairy roots", and so on).';
+            break;
+        case 'non-alphanumeric':
+            help_text = '"fer" finds entities such as "fer-tor" and "fer/ripk".';
+            break;
+        default:
+            help_text = 'Click one of the above buttons to find out more about each search function!';
+            break;
+    };
+    help_information.innerText = help_text;
+}
+
+// Attach event listeners to each button:
+buttons.forEach(button => {
+    let button_text = button.innerText.toLowerCase();
+    button.setAttribute('onclick', `change_help_text('${button_text}')`);
+})
