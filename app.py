0d9d9aa72bd75cffe64b15ae8ac16a32d151a6a9
kevfo <75442525+kevfo@users.noreply.github.com>
2023-06-11 02:59:17 +0800

    Added send_from_directory function to imports

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, send_from_directory
 import os
 
 # == IMPORTING THE ROUTES  ==
