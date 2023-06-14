4312d2e88f7b21fa174e307cd19f5e486bfdce99
kevfo <75442525+kevfo@users.noreply.github.com>
2023-06-15 01:27:26 +0800

    Now tests find_terms function

--- tests/unit/test_search_functions.py
+++ tests/unit/test_search_functions.py
@@ -1,13 +1,51 @@
-def test_search_routes_should_only_accept_get_requests(client):
-    pass
-    '''
-    Fill this out later
-    '''
-    for i in ['normal', 'exact', 'alias', 'substring', 'non_alpha']:
-        req_post, req_put = client.post(), client.put()
-        req_patch, req_del = client.patch(), client.delete()
-        
-        assert req_post.status_code == 405
-        assert req_put.status_code == 405
-        assert req_patch.status_code == 405
-        assert req_del.status_code == 405
+'''
+This file contains unit tests to test the functionality of the searching functions.
+'''
+
+from utils.search import find_terms
+
+# == TESTING VARIABLES ==
+TEST_DICT = {'cheesecake'.upper() : [('cheesecake', 'is', 'good', '1'), ('cheesecake', 'is', 'cheesy', '2'), ('cheesecake', 'is', 'great', '3')],
+             'burger'.upper() : [('burger', 'contains', 'meat', '4'), ('burger', 'complements', 'soda', '5'), ('burger', 'causes', 'obesity', '6')],
+             'chocolate'.upper() : [('chocolate', 'has', 'polyphenols', '7')],
+             'cesa1'.upper() : [('cesa1', 'is an alias of', 'cesa', '8')],
+             'chocolate1'.upper() : [('hey', 'hey', 'hey', '9')],
+             'chocolate sauce'.upper() : [('chocolate sauce', 'begets', 'ice cream', '10')]}
+
+def test_exact_search_should_return_exact_matches():
+    right, wrong = list(TEST_DICT.keys()), ['ches', 'burg', 'choco']
+    for i in right:
+        elements, forSending = find_terms(i, TEST_DICT, 'exact')
+        assert len(elements) and len(forSending)
+
+    for i in wrong:
+        elements, forSending = find_terms(i, TEST_DICT, 'exact')
+        assert not len(elements) and not len(forSending)
+
+def test_alias_search_should_return_aliases():
+    right, wrong = 'cesa1', 'blahblahblah'
+    elements, forSending = find_terms(right, TEST_DICT, 'alias')
+    assert len(elements) and len(forSending)
+
+    elements, forSending = find_terms(wrong, TEST_DICT, 'alias')
+    assert not len(elements) and not len(forSending)
+
+def test_substring_search_should_find_substrings():
+    right, wrong = ['chees', 'choco', 'bu'], ['laet', 'sehc', 'ub']
+    for i in right:
+        elements, forSending = find_terms(i, TEST_DICT, 'substring')
+        assert len(elements) and len(forSending)
+    
+    for i in wrong:
+        elements, forSending = find_terms(i, TEST_DICT, 'substring')
+        assert not len(elements) and not len(forSending)
+
+def test_nonalpha_search_should_return_nonalpha_results():
+    elements, _ = find_terms('chocolate', TEST_DICT, 'non-alphanumeric')
+    assert 'chocolate1' not in [i[0] for i in elements]
+
+def test_default_search_should_return_terms_containing_query():
+    elements, forSending = find_terms('chocolate', TEST_DICT, 'default')
+    assert len(elements) and len(forSending)
+
+
