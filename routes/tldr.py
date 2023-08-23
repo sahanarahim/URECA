c1ffaeadf630eda3c0301b21cbca85d7ee0a6917
Herman <hermanfyh@gmail.com>
2023-08-23 22:25:17 +0800

    debug

--- routes/tldr.py
+++ routes/tldr.py
@@ -29,45 +29,44 @@ def tldr_search():
         gene = "CESA1"
     else:
         gene = user_input.upper()
-    # query = '''
-    #     You are a plant molecular biology expert and systems biologist. Consider the following data about the %s. Organize these facts into a comprehensive summary structured into the following categories:
+    query = '''
+        You are a plant molecular biology expert and systems biologist. Consider the following data about the %s. Organize these facts into a comprehensive summary structured into the following categories:
 
-    #     1) Enumerate all the functions that the %s performs based on the data.
-    #     2) Identify all sub-cellular compartments (e.g, nucleus, cytosol, plasma membrane) where %s is present.
-    #     3) Specify all the sites (cells, tissues, organs) where the %s is active.
-    #     4) List all the Gene Ontology (GO) terms stated for %s. If none are stated, propose possible ones based on the data.
-    #     5) Based on the data, list all the genes that %s interacts (e.g., interacts, binds, is in complex with) with.
-    #     6) Mention all the genes that %s regulates, maintains or affectes.
-    #     7) Detail all the genes that regulates, maintains or affectes %s according to the data.
-    #     An entity refers to either a gene, molecule, compartment, stress, cell type, organ, or other related terms
-    #     For each point, if a entity is said to "interact with", "maintain", "enhance", "repress", "regulate" or "activate" another entity, consider this as an interaction. When a entity "produces" or "encodes for" a substance, consider this as a function of the entity. If an entity is "found in" a particular site, consider this as a place where the entity is expressed(gene) or located(others).
-    #     IMPORTANT Tag the CORRECT source behind each statement in parenthesis (e.g., (10024464)).
-    #     Given your prior knowledge on %s in plant biology context, Please phrase it in a coherent and plesant fashion.
-    #     Please start the reply with "Comprehensive Summary of %s"
-    #     Please start with the categorical question; followed by the answer
-    #     Please add this statement at the end "Please note that the sources are provided after each statement."
-    #     Please make sure all the information give has been covered
-    #     ''' % (gene, gene, gene, gene, gene, gene, gene, gene, gene, gene)
+        1) Enumerate all the functions that the %s performs based on the data.
+        2) Identify all sub-cellular compartments (e.g, nucleus, cytosol, plasma membrane) where %s is present.
+        3) Specify all the sites (cells, tissues, organs) where the %s is active.
+        4) List all the Gene Ontology (GO) terms stated for %s. If none are stated, propose possible ones based on the data.
+        5) Based on the data, list all the genes that %s interacts (e.g., interacts, binds, is in complex with) with.
+        6) Mention all the genes that %s regulates, maintains or affectes.
+        7) Detail all the genes that regulates, maintains or affectes %s according to the data.
+        An entity refers to either a gene, molecule, compartment, stress, cell type, organ, or other related terms
+        For each point, if a entity is said to "interact with", "maintain", "enhance", "repress", "regulate" or "activate" another entity, consider this as an interaction. When a entity "produces" or "encodes for" a substance, consider this as a function of the entity. If an entity is "found in" a particular site, consider this as a place where the entity is expressed(gene) or located(others). 
+        IMPORTANT Tag the CORRECT source behind each statement in parenthesis (e.g., (10024464)).
+        Given your prior knowledge on %s in plant biology context, Please phrase it in a coherent and plesant fashion. 
+        Please start the reply with "Comprehensive Summary of %s"
+        Please start with the categorical question; followed by the answer
+        Please add this statement at the end "Please note that the sources are provided after each statement."
+        Please make sure all the information give has been covered
+        ''' % (gene, gene, gene, gene, gene, gene, gene, gene, gene, gene)
 
-    # save = ['source\tassociation type\ttarget\tsource\n']
+    save = ['source\tassociation type\ttarget\tsource\n']
     limit = 500
     warning = ""
-    # with open("allDic3", "rb") as file:
-    #     allDic3 = pickle.load(file)
-    # for k, v in allDic3[gene[0]][gene[1]].items():
-    #     if gene == k:
-    #         save[0] += v
-    #         if save[0].count("\n") > limit:
-    #             warning = '''Network of %s is extensive, only the first %s entities that interact with %s will be displayed''' % (
-    #                 gene, limit, gene)
-    #             parts = save[0].split("\n", limit)
-    #             shorten = "\n".join(parts[:limit])
-    #             save[0] = shorten
-    #         break
+    with open("allDic3", "rb") as file:
+        allDic3 = pickle.load(file)
+    for k, v in allDic3[gene[0]][gene[1]].items():
+        if gene == k:
+            save[0] += v
+            if save[0].count("\n") > limit:
+                warning = '''Network of %s is extensive, only the first %s entities that interact with %s will be displayed''' % (
+                    gene, limit, gene)
+                parts = save[0].split("\n", limit)
+                shorten = "\n".join(parts[:limit])
+                save[0] = shorten
+            break
 
-    # messages = [{"role": "user", "content": query}]
-    # output = openai.ChatCompletion.create(model="gpt-4", messages=messages)
-
+    messages = [{"role": "user", "content": query}]
+    output = openai.ChatCompletion.create(model="gpt-4", messages=messages)
     # v = open('search.txt', 'w')
     # v.writelines(save)
     # v.close()
@@ -78,5 +77,4 @@ def tldr_search():
     # index = VectorstoreIndexCreator().from_loaders([loader])
     # output = index.query(query, llm=ChatOpenAI(
     #     temperature=0.05, model="gpt-4"))
-    # return jsonify(content=output.choices[0].message.content, warning=warning)
-    return jsonify(content=user_input, warning=warning)
+    return jsonify(content=output.choices[0].message.content, warning=warning)
